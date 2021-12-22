8b12edee160a32715e3c2c09804dad66a89b42b9
Marco Penajoia <penajoia@MPenajoias-MBP.local>
2021-12-22 13:49:11 -0800

    change redirect

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 
 app.get('/', (req, res) => {
-  res.send('Welcome to my api')
+  res.redirect('/assets')
 } )
 app.use('/assets', assetController)
 

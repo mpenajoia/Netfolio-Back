f4f0af5637472e3cfc1e656aa55a9abdadaef3a1
Marco Penajoia <penajoia@MPenajoias-MBP.local>
2021-12-22 15:33:00 -0800

    if statement moved

--- db/connections.js
+++ db/connections.js
@@ -2,14 +2,14 @@ const mongoose = require('mongoose')
 
 let mongoURI = "";
 
-mongoose.connect(mongoURI, {useNewUrlParser:true})
 
 if (process.env.NODE_ENV === "production") {
-    mongoURI = process.env.DB_URL;
-  } else {
-    mongoURI = "mongodb://localhost/assets";
-    
-  }
+  mongoURI = process.env.DB_URL;
+} else {
+  mongoURI = "mongodb://localhost/assets";
+  
+}
 
+mongoose.connect(mongoURI, {useNewUrlParser:true})
 
 module.exports = mongoose;

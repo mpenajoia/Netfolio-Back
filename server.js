ffa931ccdb4a0fb045de767c3ca82d279661f52c
Marco Penajoia <penajoia@MPenajoias-MBP.local>
2021-12-22 11:25:10 -0800

    app.get route uploaded

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 app.use('/assets', assetController)
+app.get('/', (req, res) => {
+  res.send('Welcome to my api')
+} )
 
 app.set("port", process.env.PORT || 4000);
 

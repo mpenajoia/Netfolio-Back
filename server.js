d2e18833f4bd29d3dea88bb88982744ab5d63c9f
Marco Penajoia <penajoia@MPenajoias-MBP.local>
2021-12-22 13:51:27 -0800

    redirect removed, path changed

--- server.js
+++ server.js
@@ -10,10 +10,10 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 
-app.get('/', (req, res) => {
-  res.redirect('/assets')
-} )
-app.use('/assets', assetController)
+// app.get('/', (req, res) => {
+//   res.redirect('/assets')
+// } )
+app.use('/', assetController)
 
 app.set("port", process.env.PORT || 4000);
 
